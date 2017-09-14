c272a4a06631c241a39ec37e53026cfe66ae959e
efahmad <s.ahmad.ef@gmail.com>
2017-09-14 08:32:25 +0430

    Creating Network class

--- network.py
+++ network.py
@@ -1,5 +1,16 @@
 class Node(object):
-    def __init__(self, name, num_users, adjacent_nodes):
-        self.name = name
+    def __init__(self, x, y, num_users):
+        self.x = x
+        self.y = y
         self.num_users = num_users
-        self.adjacent_nodes = adjacent_nodes
+
+
+class Network(object):
+    def __init__(self, left_boundary=0, right_boundary=1000, bottom_boundary=0, top_boundary=1000,
+                 adj_distance=250, num_nodes=100):
+        self.left_boundary = left_boundary
+        self.right_boundary = right_boundary
+        self.bottom_boundary = bottom_boundary
+        self.top_boundary = top_boundary
+        self.adj_distance = adj_distance
+        self.num_nodes = num_nodes
