28d1d2f01246f6cf42d4081f0a26174e19c3d2de
efahmad <s.ahmad.ef@gmail.com>
2017-09-15 09:17:36 +0430

    Creating nodes. - Select source nodes & destination nodes.

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from network import Network
+import matplotlib.pyplot as plt
+
+
 def main():
-    pass
+    network = Network()
+    plt.scatter([node.x for node in network.nodes], [node.y for node in network.nodes])
+    plt.scatter([node.x for node in network.source_nodes], [node.y for node in network.source_nodes])
+    plt.scatter([node.x for node in network.destination_nodes], [node.y for node in network.destination_nodes])
+    plt.show()
 
 
 if __name__ == '__main__':
--- network.py
+++ network.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class Node(object):
     def __init__(self, x, y, num_users):
         self.x = x
@@ -6,11 +9,31 @@ class Node(object):
 
 
 class Network(object):
-    def __init__(self, left_boundary=0, right_boundary=1000, bottom_boundary=0, top_boundary=1000,
-                 adj_distance=250, num_nodes=100):
+    def __init__(self, left_boundary=0.0, right_boundary=1000.0, bottom_boundary=0.0, top_boundary=1000.0,
+                 adj_distance=250, num_nodes=100, num_sources=1, num_destinations=3):
         self.left_boundary = left_boundary
         self.right_boundary = right_boundary
         self.bottom_boundary = bottom_boundary
         self.top_boundary = top_boundary
         self.adj_distance = adj_distance
         self.num_nodes = num_nodes
+        self.num_sources = num_sources
+        self.num_destinations = num_destinations
+        # ***************************************** #
+        self.nodes = []
+        self.source_nodes = []
+        self.destination_nodes = []
+        # Create the network
+        self.create()
+
+    def create(self):
+        # Get 100 random x,y between boundaries
+        x_array = np.random.uniform(low=self.left_boundary, high=self.right_boundary, size=self.num_nodes)
+        y_array = np.random.uniform(low=self.bottom_boundary, high=self.top_boundary, size=self.num_nodes)
+        for x, y in zip(x_array, y_array):
+            self.nodes.append(Node(x, y, np.random.randint(low=1, high=20)))
+        # Convert to numpy array
+        self.nodes = np.array(self.nodes)
+        # Randomly select source and destination nodes
+        self.source_nodes = self.nodes[np.random.randint(low=0, high=self.num_nodes, size=self.num_sources)]
+        self.destination_nodes = self.nodes[np.random.randint(low=0, high=self.num_nodes, size=self.num_destinations)]
